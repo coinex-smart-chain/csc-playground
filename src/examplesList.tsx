5dc83b27f4c06837ba991dd3015098e9d6c08d5f
cccCoding <826674632@qq.com>
2021-08-04 18:31:13 +0800

    update oenrpc.json link

--- src/examplesList.tsx
+++ src/examplesList.tsx
@@ -1,6 +1,6 @@
 export default [
   {
     name: "csc",
-    url: "https://raw.githubusercontent.com/coinex-smart-chain/csc-playground/adjust_csc/openrpc.json", //tslint:disable-line
+    url: "https://raw.githubusercontent.com/coinex-smart-chain/csc-playground/master/openrpc.json", //tslint:disable-line
   },
 ];
